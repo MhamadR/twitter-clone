07ecb7a333a60d0e8305741673d32ed5e031ef2b
MhamadR <mhamadraslan@gmail.com>
2022-09-17 19:00:12 +0300

    Added event listeners :js

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,10 @@
+function hideElement(elem) {
+  const element = document.querySelector(elem);
+  element.classList.add("hide");
+}
 
+function showElement(elem) {
+  const element = document.querySelector(elem);
+  console.log(element);
+  element.classList.remove("hide");
+}
