c910c2594bad18db4dee5cbaa0ed05fcdcef2a12
MhamadR <mhamadraslan@gmail.com>
2022-09-17 21:46:49 +0300

    Edited hideElemet() and showElement() :script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,9 +1,9 @@
-function hideElement(elem, className) {
+function addClass(elem, className) {
   const element = document.querySelector(elem);
   element.classList.add(className);
 }
 
-function showElement(elem, className) {
+function removeClass(elem, className) {
   const element = document.querySelector(elem);
   console.log(element);
   element.classList.remove(className);
