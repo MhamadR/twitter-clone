299eb376478117e0c6d5d949b0f2885c205ba886
malakshaer <shaermk06@gmail.com>
2022-09-17 11:21:39 +0300

    modified

--- backend/index.php
+++ backend/index.php
@@ -1,16 +1,15 @@
 <?php
     session_start();
     include "/backend/connection.php";
-    include "/backend/user.php";
-    include "/backend/tweet.php";
-    include "/backend/follow.php";
-    include "backend/block.php";
+    include "/backend/userClass.php";
+    include "/backend/tweetClass.php";
+    include "/backend/followClass.php";
+    
     global $conn;
 
     $getFromUser = new User($conn);
     $getFromTweet = new Tweet($conn);
     $getFromFollow = new Follow($conn);
-    $getFromBlock = new Block($conn);
 
     define("BASE_URL","http://localhost:8080/twitter/twitter-clone/backend/");
 
