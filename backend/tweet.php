c223fa5bbd3689ed5a54ce5aba54edad4650fc31
malakshaer <shaermk06@gmail.com>
2022-09-17 07:08:08 +0300

    correct fetch

--- backend/tweet.php
+++ backend/tweet.php
@@ -7,10 +7,10 @@ class Tweet extends User{
         $this->message = new Message($this->mysqli);
     }
     public function tweets($user_id,$number){
-        $mk = $this->mysqli->prepare("SELECT * FROM `tweets` WHERE `tweet_by` = $user_id AND `tweetBy` IN (SELECT `receiver` FROM `follow` WHERE `sender` = $user_id) ORDER BY `tweet_id` DESC LIMIT $number");
-        $mk->bind_param("ss",$user_id,$number);
-        $mk = execute();
-        $tweets = $mk->fetchALL(mysqli->fetch);
+        $query = $this->mysqli->prepare("SELECT * FROM `tweets` WHERE `tweet_by` = $user_id AND `tweetBy` IN (SELECT `receiver` FROM `follow` WHERE `sender` = $user_id) ORDER BY `tweet_id` DESC LIMIT $number");
+        $query->bind_param("ii",$user_id,$number);
+        $query = execute();
+        $tweets = $query->fetch_assoc();
 
         foreach($tweets as $tweet){
             $likes = $this->likes($user_id,$tweet->tweet_id);
@@ -18,15 +18,15 @@ class Tweet extends User{
         }
     }
     public function getUserTweets($user_id){
-        $mk = $this->mysqli->prepare("SELECT * FROM `tweets` LEFT JOIN `users` ON `tweet_by` = `user_id` WHERE `tweetBy` = $user_id ORDER BY `tweet_id` DESC");
-        $mk->bind_param("s",$user_id);
-        $mk->execute();
-        return $mk->fetchAll(mysqli->fetch);//fetch
+        $query = $this->mysqli->prepare("SELECT * FROM `tweets` LEFT JOIN `users` ON `tweet_by` = `user_id` WHERE `tweetBy` = $user_id ORDER BY `tweet_id` DESC");
+        $query->bind_param("i",$user_id);
+        $query->execute();
+        return $query->fetch_assoc();
     }
     public function addLike($user_id,$tweet_id,$get_id){
-        $mk = $this->mysqli->prepare("UPDATE `tweets` SET `likes_count` = `likes_count`+1 WHERE `tweetID` = $tweet_id");
-		$mk->bindParam("i", $tweet_id);
-		$mk->execute();
+        $query = $this->mysqli->prepare("UPDATE `tweets` SET `likes_count` = `likes_count`+1 WHERE `tweetID` = $tweet_id");
+		$query->bindParam("i", $tweet_id);
+		$query->execute();
 
 		$this->create('likes', array('likeBy' => $user_id, 'liked_on' => $tweet_id));
 	
@@ -35,37 +35,27 @@ class Tweet extends User{
 		}
     }
     public function unLike($user_id, $tweet_id, $get_id){
-		$mk = $this->mysqli->prepare("UPDATE `tweets` SET `likesCount` = `likesCount`-1 WHERE `tweetID` = $tweet_id");
-		$mk->bind_param("i", $tweet_id);
-		$mk->execute();
+		$query = $this->mysqli->prepare("UPDATE `tweets` SET `likesCount` = `likesCount`-1 WHERE `tweetID` = $tweet_id");
+		$query->bind_param("i", $tweet_id);
+		$query->execute();
 
-		$mk = $this->pdo->prepare("DELETE FROM `likes` WHERE `likeBy` = :user_id and `likeOn` = :tweet_id");
-		$mk->bind_param("ii", $user_id,$tweet_id);
-		$mk->execute(); 
+		$query = $this->pdo->prepare("DELETE FROM `likes` WHERE `likeBy` = :user_id and `likeOn` = :tweet_id");
+		$query->bind_param("ii", $user_id,$tweet_id);
+		$query->execute(); 
 	}
     public function likes($user_id, $tweet_id){
-		$mk = $this->mysqli->prepare("SELECT * FROM `likes` WHERE `like_by` = $user_id AND `likeOn` = $tweet_id");
-		$mk->bind_param("ii", $user_id, $tweet_id);
-		$mk->execute();
-		return $mk->fetch(mysqli);
+		$query = $this->mysqli->prepare("SELECT * FROM `likes` WHERE `like_by` = $user_id AND `likeOn` = $tweet_id");
+		$query->bind_param("ii", $user_id, $tweet_id);
+		$query->execute();
+		return $query->fetch_assoc();
 	}
     public function countLikes($user_id){
-		$mk = $this->mysqli->prepare("SELECT COUNT(`like_id`) AS `totalLikes` FROM `likes` WHERE `liked_by` = $user_id");
-		$mk->bindParam("i", $user_id);
-		$mk->execute();
-		$count = $mk->fetch(mysqli);
+		$query = $this->mysqli->prepare("SELECT COUNT(`like_id`) AS `totalLikes` FROM `likes` WHERE `liked_by` = $user_id");
+		$query->bindParam("i", $user_id);
+		$query->execute();
+		$count = $query->fetch(mysqli);
 		echo $count->totalLikes;
 	}
-
-
-
-
-
-
-
-
-
-
 }
 
 ?>
