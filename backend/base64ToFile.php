3580d6f4863b694d014f3288924564351b6955be
malakshaer <shaermk06@gmail.com>
2022-09-18 12:51:03 +0300

    modify the file

--- backend/base64ToFile.php
+++ backend/base64ToFile.php
@@ -3,13 +3,11 @@ header("Access-Control-Allow-Methods:POST");
 
 $jsonReceived = file_get_contents('php://input');
 
-$obj = json_decode($jsonReceived);
-$extension = $obj->ext;
-$base64 = $obj->base64;
-$filePath = "backend/Images/".uniqid().$extension;
+$body = json_decode($jsonReceived);
+$filePath = "Images/".time().".png";
 
 
-file_put_contents($filePath,base64_decode($base64));
+file_put_contents($filePath,base64_decode($body));
 
 echo("uploaded");
 
